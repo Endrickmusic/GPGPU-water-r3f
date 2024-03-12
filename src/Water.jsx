788ffbed1e34d9763d230feacd40a33677342925
Christian <mail@christianhohenbild.de>
2024-03-12 21:05:21 +0100

    add GPUComputationRenderer

--- src/Water.jsx
+++ src/Water.jsx
@@ -1,10 +1,13 @@
 import { OrbitControls } from "@react-three/drei"
 import { useFrame, useThree } from "@react-three/fiber"
 import { useRef, useMemo, useEffect } from "react"
+import { DoubleSide, Vector2 } from "three"
+import { SimplexNoise } from 'three/addons/math/SimplexNoise.js'
+import { GPUComputationRenderer } from 'three/addons/misc/GPUComputationRenderer.js'
 
 import vertexShader from "./shader/vertexShader.js"
 import fragmentShader from "./shader/fragmentShader.js"
-import { DoubleSide, Vector2 } from "three"
+
 
 import './RenderMaterial.jsx'
 
@@ -14,16 +17,25 @@ const WIDTH = 128
 // Water size in system units
 const BOUNDS = 512
 
-export default function Water(){
+export default function initWater(){
 
     const meshRef = useRef()
     const renderMat = useRef()
 
-    useEffect(() => {
+    useEffect((gl) => {
     
       // Defines
       renderMat.current.defines.WIDTH = WIDTH.toFixed( 1 )
       renderMat.current.defines.BOUNDS = BOUNDS.toFixed( 1 )
+      
+      // Creates the gpu computation class and sets it up
+
+			const gpuCompute = new GPUComputationRenderer( WIDTH, WIDTH, gl );
+
+      const heightmap0 = gpuCompute.createTexture()
+
+      fillTexture( heightmap0 )
+
     },[])
   
 
@@ -65,3 +77,27 @@ export default function Water(){
         </mesh>
    </>
   )}
+
+
+  
+  function fillTexture( texture ) {
+
+    const waterMaxHeight = 10
+
+    function noise( x, y ) {
+
+      let multR = waterMaxHeight
+      let mult = 0.025
+      let r = 0
+      for ( let i = 0; i < 15; i ++ ) {
+
+        r += multR * simplex.noise( x * mult, y * mult )
+        multR *= 0.53 + 0.025 * i
+        mult *= 1.25
+
+      }
+
+      return r
+
+    }
+  }
